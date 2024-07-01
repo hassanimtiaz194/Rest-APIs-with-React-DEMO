e6ad6a507b416fb09dfebb57d69e2c73425aae75
Hassan-Imtiaz3 <82123163+hassanimtiaz194@users.noreply.github.com>
2024-07-01 22:58:40 +0500

    Initial commit

---
+++ Backend-Node-Rest-Apis/app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const feedRoutes = require("./routes/feed");
+const authRoutes = require("./routes/auth");
+
+const app = express();
+const MONGODB_URI =
+  "mongodb+srv://himtiaz194:H12345678@cluster0.asgtm0w.mongodb.net/messages?w=majority&appName=Cluster0";
+
+// app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
+app.use(bodyParser.json()); // application/json
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+app.use("/feed", feedRoutes);
+app.use("/auth", authRoutes);
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  res.status(status).json({ message });
+});
+
+mongoose
+  .connect(MONGODB_URI)
+  .then((result) => {
+    app.listen(8080);
+  })
+  .catch((err) => console.log(err));
